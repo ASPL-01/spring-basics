11f24c0f3bd92ed8235de42d2d8dfea64c207486
Nathan Zukoff <nzukoff@gmail.com>
2017-01-12 14:50:20 +0530

    updates point.java to return double from slope

--- src/main/java/com/allstate/models/Point.java
+++ src/main/java/com/allstate/models/Point.java
@@ -1,19 +1,22 @@
-package com.allstate.models;
-import java.lang.*;
+package com.galvanize.models;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
+import java.lang.Math;
+
 
 public class Point {
+    //formula for distance = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)))
+    //formula for slope = (y1-y2)/(x1-x2)
+    //formula for y-int = y - m*x
     private int x;
     private int y;
-
-    public Point() {
-    }
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Point(){
+
     }
 
     public int getX() {
@@ -25,14 +28,32 @@ public class Point {
     }
 
     public static double distance(Point p1, Point p2){
-        return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+        double x1 = p1.x;
+        double y1 = p1.y;
+        double x2 = p2.x;
+        double y2 = p2.y;
+        return Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2));
     }
 
-    public static double slope(Point p1, Point p2){
-        return (p2.y - p1.y) / (p2.x - p1.x);
+    public static double slope(Point p1, Point p2) {
+        double x1 = p1.x;
+        double y1 = p1.y;
+        double x2 = p2.x;
+        double y2 = p2.y;
+        return (y1-y2)/(x1-x2);
     }
 
-    public static double intercept(Point p1, Point p2){
-        return p1.y - (slope(p1, p2) * p1.x);
+    public static double intercept(Point p1, Point p2) {
+        double x1 = p1.x;
+        double y1 = p1.y;
+        return y1-slope(p1,p2)*x1;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
